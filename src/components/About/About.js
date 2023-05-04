3f7e81b78b5aae7e633275f6b661185b7dc921ef
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-04 02:22:00 -0400

    component for the about section

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <div className="bio">
+        <img src="https://i.postimg.cc/wTt545RS/307159058-178958437970854-8520586148638083196-n.jpg" alt="Profile" />
+        <p>
+          I am currently completing the Georgia Professional Education Coding Bootcamp. Through this program, I am gaining a solid understanding of various programming languages, frameworks, and tools. I am excited to apply my newly acquired skills to create innovative web applications and projects that demonstrate my abilities.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
