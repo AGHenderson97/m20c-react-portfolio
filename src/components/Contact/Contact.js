acf6243af24a30da60356624304e4fd720bf8462
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-04 02:21:12 -0400

    component for the contact form

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Process form data and send an email
+    console.log(formData);
+  };
+
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" onChange={handleChange} />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+        <textarea name="message" placeholder="Message" onChange={handleChange}></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
