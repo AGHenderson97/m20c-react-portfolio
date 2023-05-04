85ad647cb13773ad46e313146a4717cd67e00618
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-04 02:55:38 -0400

    updated to include all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Header />
+      <Navigation />
+      <main>
+        <About />
+        <Portfolio />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
