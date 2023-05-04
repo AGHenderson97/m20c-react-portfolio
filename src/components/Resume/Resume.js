008a3b36647258a6ee76680e53584b909b15de2c
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-04 02:09:39 -0400

    updated link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,12 +5,12 @@ const Resume = () => {
   return (
     <section id="resume">
       <h2>Resume</h2>
-      <a href="resume_url_here" target="_blank" rel="noopener noreferrer">
+      <a href="https://docs.google.com/document/d/1bbkGjdI51qnoyCx-0R596d3kWfUB4DfS0nDdw6Sl0ZI/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
         Download Resume
       </a>
       <h3>Proficiencies</h3>
       <ul>
-        {/**/}
+        {/* proficiencies here */}
         <li>HTML & CSS</li>
         <li>JavaScript</li>
         <li>React</li>
